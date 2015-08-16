7581747a0ffd68aa5e2e8cf0df70452d4b77ff1b
Linus Unnebäck <linus@folkdatorn.se>
2015-08-16 14:03:23 +0200

    coding style: standard

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-
-var mongodb = require('mongodb');
-var Albatross = require('./lib/albatross');
+var mongodb = require('mongodb')
+var Albatross = require('./lib/albatross')
 
 module.exports = function (uri) {
-  return new Albatross(uri);
-};
+  return new Albatross(uri)
+}
 
-module.exports.BSON = mongodb.BSON;
-module.exports.Binary = mongodb.Binary;
-module.exports.Code = mongodb.Code;
-module.exports.DBRef = mongodb.DBRef;
-module.exports.Double = mongodb.Double;
-module.exports.Long = mongodb.Long;
-module.exports.MaxKey = mongodb.MaxKey;
-module.exports.MinKey = mongodb.MinKey;
-module.exports.ObjectID = mongodb.ObjectID;
-module.exports.Symbol = mongodb.Symbol;
-module.exports.Timestamp = mongodb.Timestamp;
+module.exports.BSON = mongodb.BSON
+module.exports.Binary = mongodb.Binary
+module.exports.Code = mongodb.Code
+module.exports.DBRef = mongodb.DBRef
+module.exports.Double = mongodb.Double
+module.exports.Long = mongodb.Long
+module.exports.MaxKey = mongodb.MaxKey
+module.exports.MinKey = mongodb.MinKey
+module.exports.ObjectID = mongodb.ObjectID
+module.exports.Symbol = mongodb.Symbol
+module.exports.Timestamp = mongodb.Timestamp
--- test/albatross.js
+++ test/albatross.js
@@ -1,64 +1,59 @@
+/* eslint-env mocha */
 
-var assert = require('assert');
-var mongodb = require('mongodb');
-var albatross = require('../');
+var assert = require('assert')
+var mongodb = require('mongodb')
+var albatross = require('../')
 
 describe('Albatross', function () {
-
-  var db;
+  var db
 
   before(function () {
-    db = albatross('mongodb://localhost/albatross-test');
-  });
+    db = albatross('mongodb://localhost/albatross-test')
+  })
 
   it('should connect', function (done) {
-
     if (db.connected) {
-      process.nextTick(done);
+      process.nextTick(done)
     } else {
-      db.once('connected', done);
+      db.once('connected', done)
     }
-
-  });
+  })
 
   it('should throw errors', function () {
     assert.throws(function () {
-      albatross('hgjfdkgdfgjiod');
-    });
-  });
+      albatross('hgjfdkgdfgjiod')
+    })
+  })
 
   it('should emit errors', function (done) {
     albatross('mongodb://127.0.0.1:46543').once('error', function (err) {
-      assert(err);
-      done();
-    });
-  });
+      assert(err)
+      done()
+    })
+  })
 
   it('should make ids', function () {
+    var id1 = db.id()
+    var str = id1.toHexString()
+    var id2 = db.id(str)
 
-    var id1 = db.id();
-    var str = id1.toHexString();
-    var id2 = db.id(str);
-
-    assert(id1 instanceof albatross.ObjectID);
-    assert(id2 instanceof albatross.ObjectID);
-    assert(albatross.ObjectID.isValid(str));
-    assert(id1.equals(id2));
-
-  });
+    assert(id1 instanceof albatross.ObjectID)
+    assert(id2 instanceof albatross.ObjectID)
+    assert(albatross.ObjectID.isValid(str))
+    assert(id1.equals(id2))
+  })
 
   it('should expose mongodb bson api', function () {
-    assert.equal(albatross.BSON, mongodb.BSON);
-    assert.equal(albatross.Binary, mongodb.Binary);
-    assert.equal(albatross.Code, mongodb.Code);
-    assert.equal(albatross.DBRef, mongodb.DBRef);
-    assert.equal(albatross.Double, mongodb.Double);
-    assert.equal(albatross.Long, mongodb.Long);
-    assert.equal(albatross.MaxKey, mongodb.MaxKey);
-    assert.equal(albatross.MinKey, mongodb.MinKey);
-    assert.equal(albatross.ObjectID, mongodb.ObjectID);
-    assert.equal(albatross.Symbol, mongodb.Symbol);
-    assert.equal(albatross.Timestamp, mongodb.Timestamp);
-  });
-
-});
+    assert.equal(albatross.BSON, mongodb.BSON)
+    assert.equal(albatross.Binary, mongodb.Binary)
+    assert.equal(albatross.Code, mongodb.Code)
+    assert.equal(albatross.DBRef, mongodb.DBRef)
+    assert.equal(albatross.Double, mongodb.Double)
+    assert.equal(albatross.Long, mongodb.Long)
+    assert.equal(albatross.MaxKey, mongodb.MaxKey)
+    assert.equal(albatross.MinKey, mongodb.MinKey)
+    assert.equal(albatross.ObjectID, mongodb.ObjectID)
+    assert.equal(albatross.Symbol, mongodb.Symbol)
+    assert.equal(albatross.Timestamp, mongodb.Timestamp)
+  })
+})
